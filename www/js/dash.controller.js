a7b84d55f8801b7aa6036c03d8df89da70071067
Joe Danks <joed.traxter@gmail.com>
2017-02-20 21:48:46 -0600

    storing selected truck id to local storage to persist through refreshes. Got the pick up location to work on browser. still need to test on device. Going to look at buttons next.

--- www/js/dash.controller.js
+++ www/js/dash.controller.js
@@ -7,11 +7,22 @@ angular.module('starter.controllers', [])
     TruckService.getTruckIds().then((response) => {
       this.trucks = response.data;
       console.log(response);
+      if(localTruckId) {
+        this.selectedTruckId = localTruckId;
+        this.updateSelectedTruck();
+      }
     });
 
     this.updateSelectedTruck = () => {
       CallService.getActiveCall(this.selectedTruckId).then((response) => {
+        localTruckId = this.selectedTruckId;
+        window.localStorage.setItem('truckId', this.selectedTruckId);
         this.activeCall = response.data;
+        if(navigator.userAgent.match(/(Android)/)) {
+          this.activeCall.pickUpURL = "geo:?q="+encodeURIComponent(this.activeCall.pickUpLocation);
+        } else {
+          this.activeCall.pickUpURL = "http://maps.google.com?q="+encodeURIComponent(this.activeCall.pickUpLocation);
+        }
       });
     };
 
